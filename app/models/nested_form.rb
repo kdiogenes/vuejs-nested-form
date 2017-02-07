9cb8efb33c00cd8d3403ec01eaae7163ce008214
Kadu Diógenes <kadu@fnix.com.br>
2017-02-07 18:17:07 -0200

    Adding index_errors to active_type

--- app/models/nested_form.rb
+++ app/models/nested_form.rb
@@ -1,5 +1,5 @@
 class NestedForm < ActiveType::Object
-  nests_many :categories, allow_destroy: true
+  nests_many :categories, allow_destroy: true, index_errors: true
   nests_many :listener_deadlines, allow_destroy: true, scope: proc { Deadline }
   nests_many :presenter_deadlines, allow_destroy: true, scope: proc { Deadline }
 
